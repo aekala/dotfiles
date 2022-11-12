a0cd4eeb9db62fef8dcca87bad59c80db30c8669
aekala <lkkodish@gmail.com>
2022-11-12 12:23:40 -0800

    added polybar

--- assembleConfigs.sh
+++ assembleConfigs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-configList=('alacritty' 'bspwm' 'rofi' 'sxhkd')
+configList=('alacritty' 'bspwm' 'polybar' 'rofi' 'sxhkd')
 for i in "${configList[@]}"; do
   rm -rf ./$i
   cp -r ~/.config/$i ./$i
