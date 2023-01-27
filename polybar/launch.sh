c7c3ec8485ef109fce49568043ab8656c8f133cd
aekala <lkkodish@gmail.com>
2023-01-27 14:17:14 -0800

    updates to handle second ultrawide monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+killall -q polybar
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use 
 polybar-msg cmd quit
@@ -8,6 +9,10 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar -c $HOME/.config/polybar/config.ini mybar 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar -c $HOME/.config/polybar/config.ini primary 2>&1 | tee -a /tmp/polybar1.log & disown
+my_laptop_external_monitor = $(xrandr --query | grep 'VGA-1')
+#if [[ $my_laptop_external_monitor = *connected* ]]; then
+polybar -c $HOME/.config/polybar/config.ini secondary 2>&1 | tee -a /tmp/polybar1.log & disown
+#fi
 
 echo "Bars launched..."
