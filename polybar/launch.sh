df3e80df252acdfe95cd882f2157ceea8c1da186
aekala <lkkodish@gmail.com>
2023-02-25 01:50:21 -0800

    polybar fix for second monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,6 +8,6 @@ polybar-msg cmd quit
 # killall -q polybar
 
 polybar -c $HOME/.config/polybar/config.ini primary 2>&1 | tee -a /tmp/polybar1.log & disown
-
+polybar -c $HOME/.config/polybar/config.ini secondary 2>&1 | tee -a /tmp/polybar1.log & disown
 printf "Polybar launched...\n\n"
 cat /tmp/polybar.log
