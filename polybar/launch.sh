271cc2fdba73b4423beee889c6e2d768e8f5f6fe
aekala <lkkodish@gmail.com>
2023-02-25 00:34:10 -0800

    new polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,12 +7,7 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar -c $HOME/.config/polybar/config.ini primary 2>&1 | tee -a /tmp/polybar1.log & disown
-my_laptop_external_monitor = $(xrandr --query | grep 'VGA-1')
-#if [[ $my_laptop_external_monitor = *connected* ]]; then
-polybar -c $HOME/.config/polybar/config.ini secondary 2>&1 | tee -a /tmp/polybar1.log & disown
-#fi
 
-echo "Bars launched..."
+printf "Polybar launched...\n\n"
+cat /tmp/polybar.log
